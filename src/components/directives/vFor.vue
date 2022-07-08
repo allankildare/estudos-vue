e6031bae8695d1dea762519da2ab0ce51b71ecbe
Allan Kildare <allankildaredev@gmail.com>
2022-07-08 02:30:48 -0300

    feat: vFor component

---
+++ src/components/directives/vFor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vFor',
+  data() {
+    return {
+      items: ['Item 1', 'Item 2', 'Item 3'],
+    }
+  },
+}
+</script>
