40aff10a910d30fbbf2f2c1b8fc29f40fb7108f9
Allan Kildare <allankildaredev@gmail.com>
2022-07-08 02:31:36 -0300

    feat: adding vIf component

---
+++ src/components/directives/vIf.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Você gosta de café?</h1>
+    <input v-model="coffee" value="sim" type="radio" id="sim" />
+    <label for="sim">Simm!!</label>
+
+    <input v-model="coffee" value="nao" type="radio" id="nao" />
+    <label for="nao">Não</label>
+
+    <p v-if="coffee === 'sim'">Boaa, é muuito bom né?</p>
+    <p v-else-if="coffee === 'nao'">Como você consegue?? 😩</p>
+</template>
+
+<script>
+
+export default {
+    name: 'vIf',
+    data() {
+        return {
+            coffee: ''
+        }
+    }
+}
+</script>
