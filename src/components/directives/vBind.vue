921dcf81d2e0337d71f426ff66d1544b92133fe9
Allan Kildare <allankildaredev@gmail.com>
2022-07-08 02:32:02 -0300

    feat: adding vBind component

---
+++ src/components/directives/vBind.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>Para deixar o botão verdinho você deve digitar "Eu amo Vue"</p>
+  <span>Se é verdade ou não, veremos depois ;)</span>
+  <br />
+  <input type="text" id="loveVue" v-model="loveVue" />
+  <button :class="[loveVue === 'Eu amo Vue' ? 'green' : '']">
+    {{
+      loveVue === 'Eu amo Vue'
+        ? 'Que satisfatório né?'
+        : 'Deixe-me verde pufavô 🥺'
+    }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'vBind',
+  data() {
+    return {
+      loveVue: '',
+    }
+  },
+}
+</script>
+
+<style scoped>
+button {
+  background-color: #ccc;
+}
+.green {
+  background-color: #1b9608;
+}
+</style>
