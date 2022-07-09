d9386d3432fb39ef4764310a7f8c1f0b85dd1002
Allan Kildare <allankildaredev@gmail.com>
2022-07-09 18:31:36 -0300

    feat: adding vOn component

---
+++ src/components/directives/vOn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- use v-on ou simplesmente @ -->
+    <button v-on:click="counter > 0 ? (counter -= 1) : 0">-</button>
+    <span>Quantidade: {{ counter }}</span>
+    <button @click="counter += 1">+</button>
+    <!-- multiple bindings -->
+    <!-- <div v-on="
+        mousedown: facaIsso,
+        mouseup: facaAquilo
+    ">Clique</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vOn',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+}
+</script>
