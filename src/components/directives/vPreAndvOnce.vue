1cafcb03971df32857a41ad5699fa98ea4f0c132
Allan Kildare <allankildaredev@gmail.com>
2022-07-09 18:32:41 -0300

    feat: adding vPreAndvOnce component

---
+++ src/components/directives/vPreAndvOnce.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3>Altere o texto dentro de tapioca</h3>
+  <input type="text" v-model="tapioca" />
+
+  <p v-once>Renderizando com v-once: "{{ tapioca }}"</p>
+  <span v-pre>Vamos observar como renderiza o v-pre: {{ tapioca }}</span>
+  <p>Agora usando a tag HTML pre</p>
+  <pre>{{ $data }}</pre>
+</template>
+
+<script>
+export default {
+  name: 'vPreAndvOnce',
+  data() {
+    return {
+      tapioca: 'é muito boa com um queijinho',
+    }
+  },
+}
+</script>
