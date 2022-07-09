3ff84167263d42159b1f8d1053db6f64f2fa1c5d
Allan Kildare <allankildaredev@gmail.com>
2022-07-09 18:32:00 -0300

    feat: adding vText component

---
+++ src/components/directives/vText.vue
@@ -0,0 +1,22 @@
+<template>
+  <input type="text" v-model="tea" />
+  <p>Renderizado com v-text</p>
+  <p v-text="tea"></p>
+  <p>Renderizado com mustache</p>
+  <p>{{ tea }}</p>
+</template>
+
+<script>
+export default {
+    name: 'vText',
+    data() {
+        return {
+            tea: 'Eu gosto muito de chá de hibisco'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
