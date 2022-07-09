266df28706fd704f6bf040437eba1745d055f181
Allan Kildare <allankildaredev@gmail.com>
2022-07-09 18:33:02 -0300

    feat: adding methods component

---
+++ src/components/methods-computed-props-and-watchers/methods.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @mousemove="xCoordinate" :style="{ backgroundColor: `hsl(${x}, 80%, 50%)` }">
+  <p>Pixels no eixo X: {{ x }}</p>
+  <button @click="decrement">-</button>
+  <span>{{ counter }}</span>
+  <button @click="increment">+</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'methods',
+    data() {
+        return {
+            counter: 0,
+            x: ''
+        }
+    },
+    methods: {
+        increment() {
+            this.counter += 1
+        },
+        decrement() {
+            this.counter -= 1
+        },
+        xCoordinate(event) {
+            this.x = event.clientX
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
