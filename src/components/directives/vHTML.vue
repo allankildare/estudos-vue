501490f7ec8c41d9bd3ecccdb8bd0559b9b55e9b
Allan Kildare <allankildaredev@gmail.com>
2022-07-09 18:32:17 -0300

    feat: adding vHTML component

---
+++ src/components/directives/vHTML.vue
@@ -0,0 +1,18 @@
+<template>
+  <p v-html="github"></p>
+</template>
+
+<script>
+export default {
+    name: 'vHTML',
+    data() {
+        return {
+            github: 'Acesse meu GitHub <a href="https://github.com/allankildare" target="_blank">Allan Kildare</a>'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
