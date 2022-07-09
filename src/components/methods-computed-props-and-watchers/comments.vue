fb2ab5ee76738049929eb13d4cc699031e7428f0
Allan Kildare <allankildaredev@gmail.com>
2022-07-09 18:33:20 -0300

    feat: adding comments component

---
+++ src/components/methods-computed-props-and-watchers/comments.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <li v-for="comment in comments" :key="comment">
+      {{ comment }}
+    </li>
+  </ul>
+
+  <input
+    @keyup.enter="addNewComment"
+    type="text"
+    name="comment"
+    placeholder="Digite aqui seu comentário..."
+    v-model="newComment"
+  />
+</template>
+
+<script>
+export default {
+  name: 'comments',
+  data() {
+    return {
+    
+    newComment: '',
+      comments: [
+        'Este repositório traz conceitos importantes do Vue',
+        'Os métodos são coisas incríveis',
+      ],
+    }
+  },
+  methods: {
+    addNewComment() {
+        this.comments.push(this.newComment)
+        this.newComment = ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
